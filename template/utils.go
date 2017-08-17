79a72b0185c88b64b24984b20c1557c59c71bf76
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-08-17 18:27:02 +0900

    colors on Windows

--- template/utils.go
+++ template/utils.go
@@ -2,9 +2,13 @@ package template
 
 import (
 	"fmt"
+
+	"github.com/mattn/go-colorable"
 	"github.com/mgutz/ansi"
 )
 
+var out = colorable.NewColorableStdout()
+
 func printAction(color string, action string, target string) {
-	fmt.Printf("	%s  %s\n", ansi.Color(action, color), target)
+	fmt.Fprintf(out, "	%s  %s\n", ansi.Color(action, color), target)
 }
